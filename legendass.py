3082ea80cccb7d9c7c8e7dfee233d5838aaf21a5
DO <colin-one@hotmail.com>
2016-12-08 14:35:57 +0100

    first

---
+++ legendass.py
@@ -0,0 +1,15 @@
+import os
+import re
+
+class Episode(object):
+    EPISODE_PATH = "Z:\\LL_prod\\"
+    _episodes = []
+    @staticmethod
+    def get_episodes(self):
+        for dir in os.listdir(Episode.EPISODE_PATH):
+
+
+
+
+
+
